aa2dbfde25289b0fff7c3b9de1bb3ec125458f5b
Lee Dong Bok <ehdqhr3408@naver.com>
2020-09-17 11:05:44 +0900

    make add2 using an array

--- src/com/newlecture/web/Add2.java
+++ src/com/newlecture/web/Add2.java
@@ -6,10 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet("/add")
-public class add extends HttpServlet {
+@WebServlet("/add2")
+public class Add2 extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -17,31 +16,14 @@ public class add extends HttpServlet {
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=UTF-8");
 
-        String x_ = req.getParameter("x");
-        String y_ = req.getParameter("y");
+        String[] num_ = req.getParameterValues("num");
 
-        int x = 0;
-        int y = 0;
-
-        if(!x_.equals("")) x = Integer.parseInt(x_);
-        if(!y_.equals("")) y = Integer.parseInt(y_);
-
-        int result = x+y;
+        int result = 0;
+        for (int i=0;i<num_.length; i++){
+            int num = Integer.parseInt(num_[i]);
+            result += num;
+        }
 
         resp.getWriter().printf("result is %d\n", result);
-
-        /*내가 만든 덧셈계산기*/
-
-        /* resp.setCharacterEncoding("UTF-8");
-        resp.setContentType("text/html; charset=UTF-8");
-
-        PrintWriter out = resp.getWriter();
-
-        int num1 = Integer.parseInt(req.getParameter("x"));
-        int num2 = Integer.parseInt(req.getParameter("y"));
-
-        int sum = num1 + num2;
-
-        out.println("계산 결과 : " + sum);*/
     }
 }
