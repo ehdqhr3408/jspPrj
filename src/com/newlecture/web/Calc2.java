8762b9cdffa27547692f6bad9d6dfc665c6f4273
Lee Dong Bok <ehdqhr3408@naver.com>
2020-09-17 13:28:19 +0900

    application storage test

---
+++ src/com/newlecture/web/Calc2.java
@@ -0,0 +1,52 @@
+package com.newlecture.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/calc")
+public class Calc extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        /*답안지*/
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+
+        String x_ = req.getParameter("x");
+        String y_ = req.getParameter("y");
+        String op = req.getParameter("operater");
+
+        int x = 0;
+        int y = 0;
+
+        if (!x_.equals("")) x = Integer.parseInt(x_);
+        if (!y_.equals("")) y = Integer.parseInt(y_);
+
+        int result = 0;
+
+        if (op.equals("덧셈"))
+            result = x + y;
+        else if (op.equals("뺄셈"))
+            result = x - y;
+
+        resp.getWriter().printf("result is %d\n", result);
+
+        /*내가 만든 덧셈계산기*/
+
+        /* resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+
+        PrintWriter out = resp.getWriter();
+
+        int num1 = Integer.parseInt(req.getParameter("x"));
+        int num2 = Integer.parseInt(req.getParameter("y"));
+
+        int sum = num1 + num2;
+
+        out.println("계산 결과 : " + sum);*/
+    }
+}
