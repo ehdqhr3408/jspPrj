e43187fad5696dd2aad909024749078db71d5089
Lee Dong Bok <ehdqhr3408@naver.com>
2020-09-17 15:39:02 +0900

    cookie path and maxage test

--- src/com/newlecture/web/Calc2.java
+++ src/com/newlecture/web/Calc2.java
@@ -3,11 +3,9 @@ package com.newlecture.web;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
+import java.lang.invoke.ConstantCallSite;
 
 @WebServlet("/calc2")
 public class Calc2 extends HttpServlet {
@@ -18,6 +16,7 @@ public class Calc2 extends HttpServlet {
         HttpSession session = req.getSession();
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=UTF-8");
+        Cookie[] cookies = req.getCookies();
 
         String v_ = req.getParameter("v");
         String op = req.getParameter("operater");
@@ -30,14 +29,28 @@ public class Calc2 extends HttpServlet {
             // 계산
 
             //int x = (Integer)application.getAttribute("value");
-            int x = (Integer)session.getAttribute("value");
+            //int x = (Integer)session.getAttribute("value");
+            int x = 0;
+            for(Cookie c : cookies){
+                if(c.getName().equals("value")){
+                    x = Integer.parseInt(c.getValue());
+                    break;
+                }
+            }
+
             int y = v;
             //String operater = (String)application.getAttribute("op");
-            String operater = (String)session.getAttribute("op");
-
+            //String operater = (String)session.getAttribute("op");
+            String operator ="";
+            for(Cookie c : cookies){
+                if(c.getName().equals("op")){
+                    operator = c.getValue();
+                    break;
+                }
+            }
             int result = 0;
 
-            if (operater.equals("+"))
+            if (operator.equals("+"))
                 result = x + y;
             else
                 result = x - y;
@@ -48,9 +61,21 @@ public class Calc2 extends HttpServlet {
             // application 저장소에 저장
             //application.setAttribute("value", v);
             //application.setAttribute("op", op);
-            session.setAttribute("value", v);
-            session.setAttribute("op", op);
 
+            //session애 저장
+            //session.setAttribute("value", v);
+            //session.setAttribute("op", op);
+
+            Cookie valueCookie = new Cookie("value", String.valueOf(v));
+            valueCookie.setPath("calc2");
+            valueCookie.setMaxAge(24*60*60);
+
+            Cookie opCookie = new Cookie("op", op);
+            opCookie.setPath("/calc2");
+            valueCookie.setMaxAge(24*60*60);
+
+            resp.addCookie(valueCookie);
+            resp.addCookie(opCookie);
         }
     }
 
